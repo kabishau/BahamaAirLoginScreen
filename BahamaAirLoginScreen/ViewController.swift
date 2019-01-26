14ddbce49929596fd57a8537b280bc795241be64
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-26 13:40:44 -0500

    making spinner visible and loginButton change background

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -118,7 +118,11 @@ class ViewController: UIViewController {
     
     UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
         self.loginButton.center.y += 60.0
+        self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.85, blue: 0.45, alpha: 1.0)
+        self.spinner.center = CGPoint(x: 40.0, y: self.loginButton.frame.size.height / 2)
+        self.spinner.alpha = 1.0
     }, completion: nil)
+    
   }
 
   // MARK: UITextFieldDelegate
